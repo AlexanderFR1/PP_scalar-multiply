bf17d69f9a4b2bf36c15efdc91440ce3c0826424
AlexanderFR1 <ForOriginLog@yandex.ru>
2017-10-18 16:40:17 +0300

    workable 1.0

--- PP_1_9_01/main.cpp
+++ PP_1_9_01/main.cpp
@@ -1,3 +1,4 @@
+#define VECSIZE 3
 #include <mpi.h> 
 #include <iostream> 
 #include <vector> 
@@ -5,20 +6,24 @@ using namespace std;
 
 int main(int argc, char **argv) 
 { 
-	double sumA, sumB,tempA,tempB,res;
+	double sumA, sumB,tempA=0,tempB=0,res,wew;
 	int ProcRank, coutOfProcs,i; 
-	int n=4;
-	vector<int> a(4),b(4); 
-	int cos; 
+	int n=VECSIZE;
+	vector<int> a(VECSIZE),b(VECSIZE); 
+	double cos; 
 	cout<<"vector a:\n"; 
-	for (int j=0;j<4;j++)
+	for (int j=0;j<VECSIZE;j++)
 		cin>>a[j];
 	cout<<"vector b:\n"; 
-	for (int j=0;j<4;j++)
+	for (int j=0;j<VECSIZE;j++)
 		cin>>b[j];
 	cout<<"cosinus:\n"; 
 	cin>>cos; 
-
+	if((cos>1)||(cos<-1))
+	{
+		cout<<"Wrong cosinus\n";
+		return 0;
+	}
 	MPI_Init(&argc, &argv); 
 	MPI_Comm_size(MPI_COMM_WORLD,&coutOfProcs); 
 	MPI_Comm_rank(MPI_COMM_WORLD,&ProcRank); 
@@ -27,15 +32,17 @@ int main(int argc, char **argv)
 	sumB=0.0;
 	for (i = ProcRank + 1; i <= n; i += coutOfProcs) 
 	{
-		tempA+=a[i-coutOfProcs]*a[i-coutOfProcs];
-		tempB+=b[i-coutOfProcs]*a[i-coutOfProcs];
+		tempA+=a[ProcRank]*a[ProcRank];
+		tempB+=b[ProcRank]*b[ProcRank];
 	}
 	MPI_Reduce(&tempA,&sumA,1,MPI_DOUBLE,MPI_SUM,0,MPI_COMM_WORLD);
 	MPI_Reduce(&tempB,&sumB,1,MPI_DOUBLE,MPI_SUM,0,MPI_COMM_WORLD);
 	if(ProcRank==0)
 	{
 		res=sqrt(sumA)*sqrt(sumB)*cos;
-		cout<<"a*b = "<<res<<"\n";
+		cout<<"a*b = "<<res<<"\n";		
 	}
 	MPI_Finalize();
+	cin>>wew;
+	return 0;
 }
