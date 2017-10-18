9d53fe4802bc76f6b8007dacdb95783db90aced4
AlexanderFR1 <ForOriginLog@yandex.ru>
2017-10-18 22:03:25 +0300

    Workable 1.0.1

--- PP_1_9_01/main.cpp
+++ PP_1_9_01/main.cpp
@@ -1,3 +1,4 @@
+#define VECSIZE 3
 #include <mpi.h> 
 #include <iostream> 
 #include <vector> 
@@ -5,37 +6,53 @@ using namespace std;
 
 int main(int argc, char **argv) 
 { 
-	double sumA, sumB,tempA,tempB,res;
+	double sumA, sumB,tempA=0.0,tempB=0.0,res,wew;
 	int ProcRank, coutOfProcs,i; 
-	int n=4;
-	vector<int> a(4),b(4); 
-	int cos; 
-	cout<<"vector a:\n"; 
-	for (int j=0;j<4;j++)
-		cin>>a[j];
-	cout<<"vector b:\n"; 
-	for (int j=0;j<4;j++)
-		cin>>b[j];
-	cout<<"cosinus:\n"; 
-	cin>>cos; 
-
+	int n=VECSIZE;
+	int done=0;
+	sumA=0.0;
+	sumB=0.0;
+	vector<int> a(VECSIZE),b(VECSIZE); 
+	double cos; 
+	
 	MPI_Init(&argc, &argv); 
 	MPI_Comm_size(MPI_COMM_WORLD,&coutOfProcs); 
 	MPI_Comm_rank(MPI_COMM_WORLD,&ProcRank); 
-	MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
-	sumA=0.0;
-	sumB=0.0;
-	for (i = ProcRank + 1; i <= n; i += coutOfProcs) 
-	{
-		tempA+=a[i-coutOfProcs]*a[i-coutOfProcs];
-		tempB+=b[i-coutOfProcs]*a[i-coutOfProcs];
-	}
-	MPI_Reduce(&tempA,&sumA,1,MPI_DOUBLE,MPI_SUM,0,MPI_COMM_WORLD);
-	MPI_Reduce(&tempB,&sumB,1,MPI_DOUBLE,MPI_SUM,0,MPI_COMM_WORLD);
-	if(ProcRank==0)
-	{
-		res=sqrt(sumA)*sqrt(sumB)*cos;
-		cout<<"a*b = "<<res<<"\n";
-	}
+
+		if(ProcRank==0)
+		{
+			cout<<"vector a:\n"; 
+			for (int j=0;j<VECSIZE;j++)
+				cin>>a[j];
+			cout<<"vector b:\n"; 
+			for (int j=0;j<VECSIZE;j++)
+				cin>>b[j];
+			cout<<"cosinus:\n"; 
+			cin>>cos; 
+			if((cos>1)||(cos<-1))
+			{
+				cout<<"Wrong cosinus\n";
+				MPI_Finalize();
+				return 0;
+			}
+		}
+		MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	
+		for (i = ProcRank + 1; i <= n; i += coutOfProcs) //почему-то всё выполняется на одном процессоре
+		{
+			tempA+=a[ProcRank]*a[ProcRank];
+			tempB+=b[ProcRank]*b[ProcRank];
+		}
+		MPI_Reduce(&tempA,&sumA,1,MPI_DOUBLE,MPI_SUM,0,MPI_COMM_WORLD);
+		MPI_Reduce(&tempB,&sumB,1,MPI_DOUBLE,MPI_SUM,0,MPI_COMM_WORLD);
+		if(ProcRank==0)
+		{
+			res=sqrt(sumA)*sqrt(sumB)*cos;
+			cout<<"a*b = "<<res<<"\n";		
+		}		
+	
 	MPI_Finalize();
+	cin>>wew;
+	return 0;
 }
+
